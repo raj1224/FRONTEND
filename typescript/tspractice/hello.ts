f9f79dd9261e7f4ebcc64854a4e789bcd0f70b10
Raj Kumar <imraaj1026@gmail.com>
2025-09-06 14:23:41 +0530

    ts lec 1 completed

---
+++ typescript/tspractice/hello.ts
@@ -0,0 +1,8 @@
+// console.log('hello world');
+
+let message:string = 'hello world';
+message="10";
+console.log(message);
+
+// temporary error removal
+export{};
---
+++ typescript/tspractice/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 21;
+console.log(age);
+//# sourceMappingURL=index.js.map
