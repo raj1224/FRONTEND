3c7d3f6dcf0c459b45c63c419de8ed7afcd11c49
Raj Kumar <imraaj1026@gmail.com>
2025-09-18 21:41:19 +0530

    lec 1 of nextjs done

---
+++ Nextjsprac/my-app/app/products/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = async({params}:{params:Promise<{id:string}>})=> {
+    const {id} = await params;
+  return (
+    <div>page : {id}</div>
+  )
+}
+
+export default page
